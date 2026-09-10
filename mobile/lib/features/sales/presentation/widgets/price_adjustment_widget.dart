f54f76b278bf9608ab91c54e22761d1b9330ee06
JohnHika <johnkimani576@gmail.com>
2026-09-10 23:26:54 +0300

    fix(ui): import depth for price_adjustment_widget (resolves from widgets/ dir)

--- mobile/lib/features/sales/presentation/widgets/price_adjustment_widget.dart
+++ mobile/lib/features/sales/presentation/widgets/price_adjustment_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../core/theme/design_system.dart';
+import '../../../../core/theme/design_system.dart';
 
 import '../providers/cart_provider.dart';
 
